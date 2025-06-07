eb4322c337a4220fe0fff97f6ee516f7f21a8ecb
Muhammed Hassan <m.hassananwer@gmail.com>
2025-06-07 11:19:05 +0300

    fixing https

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  httpsAgent: new https.Agent({ keepAlive: true }),
   headers: {
     "Content-Type": "application/json",
   },
